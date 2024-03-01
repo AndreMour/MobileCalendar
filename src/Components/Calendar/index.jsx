2b1acccef563a0518c89f56efb32f8050db9dac4
AndreMour <andreluizmoura2004@outlook.com>
2024-03-01 12:02:15 -0300

    Implement Calendar

---
+++ src/Components/Calendar/index.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Body,
+    DayWeek,
+    DaysOfTheWeek,
+    Months,
+    Month,
+    ArrowIcon,
+    DaysOfTheMonth,
+    Day,
+    DayNumber,
+    WeekDay,
+    AlignBody
+} from '../Calendar/styles';
+
+function getAllFridays(year, startMonth = 0, endMonth = 11) {
+    const fridays = [];
+
+    for (let month = startMonth; month <= endMonth; month++) {
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+        for (let day = 1; day <= daysInMonth; day++) {
+            const date = new Date(year, month, day);
+
+            if (date.getDay() === 5) {
+                fridays.push(date);
+            }
+        }
+    }
+
+    return fridays;
+}
+
+export default function Calendar({ fridayGroups }) {
+    const DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const DAYS_OF_THE_WEEK = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'];
+    const MONTHS = [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro',
+    ];
+
+    const today = new Date();
+    const [currentDate, setCurrentDate] = useState(today);
+    const [day, setDay] = useState(currentDate.getDate());
+    const [month, setMonth] = useState(currentDate.getMonth());
+    const [year, setYear] = useState(currentDate.getFullYear());
+    const [startDay, setStartDay] = useState(getStartDayOfMonth(currentDate));
+
+    const days = isLeapYear(year) ? DAYS_LEAP : DAYS;
+
+    function getStartDayOfMonth(date) {
+        const startDate = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+        return startDate === 0 ? 7 : startDate;
+    }
+
+    function isLeapYear(year) {
+        return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    }
+
+    useEffect(() => {
+        setDay(currentDate.getDate());
+        setMonth(currentDate.getMonth());
+        setYear(currentDate.getFullYear());
+        setStartDay(getStartDayOfMonth(currentDate));
+    }, [currentDate]);
+
+    const allFridays = getAllFridays(year, 0, 11);
+
+    return (
+        <Body>
+            <Months>
+                <ArrowIcon
+                    name="arrow-left"
+                    onPress={() => setCurrentDate(new Date(year, month - 1, day))}
+                />
+                <Month>{MONTHS[month]} </Month>
+                <ArrowIcon
+                    name="arrow-right"
+                    onPress={() => setCurrentDate(new Date(year, month + 1, day))}
+                />
+            </Months>
+            <AlignBody>
+                <DaysOfTheWeek>
+                    {DAYS_OF_THE_WEEK.map((d, index) => (
+                        <DayWeek key={index}>
+                            <WeekDay>{d}</WeekDay>
+                        </DayWeek>
+                    ))}
+                </DaysOfTheWeek>
+                {Array(Math.ceil((days[month] + startDay - 1) / 7))
+                    .fill(null)
+                    .map((_, rowIndex) => (
+                        <DaysOfTheMonth
+                            key={rowIndex}
+                            style={{
+                                borderTopWidth: 1,
+                                borderTopColor: "#313131"
+                            }}
+                        >
+                            {Array(7)
+                                .fill(null)
+                                .map((_, colIndex) => {
+                                    const dayIndex = rowIndex * 7 + colIndex + 1 - startDay;
+                                    const d = dayIndex > 0 && dayIndex <= days[month] ? dayIndex : null;
+                                    const dayOfWeek = (colIndex + startDay - 1) % 7;
+                                    return (
+                                        <Day
+                                            key={colIndex}
+                                            isToday={d === today.getDate()}
+                                            isSelected={d === day}
+                                            empty={!d}
+                                            dayOfWeek={dayOfWeek}
+                                        >
+                                            <DayNumber>{d > 0 ? String(d).padStart(2, '0') : ''}</DayNumber>
+                                        </Day>
+                                    );
+                                })}
+                        </DaysOfTheMonth>
+                    ))}
+            </AlignBody>
+        </Body>
+    );
+}
---
+++ src/Components/Calendar/styles.js
@@ -0,0 +1,105 @@
+import styled, { css } from "styled-components/native";
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+
+export const Body = styled.View`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #fff;
+`
+export const Months = styled.View` 
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+  width: 100%;
+`
+export const Month = styled.Text`
+  font-family: 'Montserrat-SemiBold';
+  font-size: 20px;
+  font-weight: 600;
+  color: #0F0F0F;
+`
+export const ArrowIcon = styled(SimpleLineIcons)`
+  color: #0F0F0F;
+  font-size: 15px;
+  padding: 0 45px 0 45px;
+`
+
+export const DaysOfTheWeek = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+  height: 8vh;
+  padding: 20px 0 15px 0;
+`
+export const DayWeek = styled.View`
+  display: flex;
+  align-items: flex-end;
+  padding: 8px;
+`
+export const WeekDay = styled.Text`
+  font-family: 'Montserrat-Medium';
+  font-size: 15px;
+  font-weight: 500;
+  color: #626262;
+`
+export const AlignBody = styled.View`
+  display: flex;
+`
+
+export const DaysOfTheMonth = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: calc(100% - 20px);
+  margin: 0 10px;
+  padding-left: 35px;
+  justify-content: space-between;
+ 
+`;
+
+export const Day = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  width: calc(100% / 7); 
+  height: 80px; 
+  padding-bottom: 35px;
+`;
+
+export const DayNumber = styled.Text`
+  display: flex;
+  flex-direction: row;
+  color: white;
+  font-size: 16px;
+  font-weight: 500;
+  color: #0F0F0F;
+`;
+
+
+export const DayContent = styled.View`
+  position: absolute;
+  display: flex;
+  bottom: -10px;
+  margin-left: 10px; 
+`
+export const ParticipantsContainer = styled.View`
+  display: flex;
+  flex-wrap: wrap;
+  overflow: hidden;
+  margin: 0 50px 27px 0;
+`
+
+export const Participants = styled.Text`
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  margin: 10px 35px 10px 0;
+  color: ${(props) => props.theme.text};
+`
