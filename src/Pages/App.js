6bc5884e31a8a3010289fd87abcfa7b147c6fc04
AndreMour <andreluizmoura2004@outlook.com>
2024-03-01 12:05:19 -0300

    Refactor App

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import { Container, DivHeader, Header, Title, DivTitle, CalendarGrid } from '../Pages/Styles';
-import Feather from 'react-native-vector-icons/Feather';
-import Calendar from '../Components/Calendar';
+import SideBar from '../Components/SideBar';
 
 export default function App() {
+
   return (
-    <Container>
-      <Header>
-        <DivHeader>
-          <Feather name='menu' style={{ color: 'white', fontSize: 24 }} />
-        </DivHeader>
-        <DivTitle>
-          <Title>
-            Calendário de Limpeza
-          </Title>
-        </DivTitle>
-      </Header>
-      <CalendarGrid>
-        <Calendar />
-      </CalendarGrid>
-    </Container>
+    <SideBar />
   )
 }
 
